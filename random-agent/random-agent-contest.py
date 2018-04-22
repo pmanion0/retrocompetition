14f971f48965c41351c2998b3eb435c283f121f3
Patrick Manion <patrickjmanion@gmail.com>
2018-04-22 17:29:19 -0500

    Changes to allow local/remote random agent execution

--- random-agent/random-agent-contest.py
+++ random-agent/random-agent-contest.py
@@ -1,12 +1,31 @@
-from retro_contest.local import make
+import sys
+
+is_local = False
+
+if len(sys.argv) > 1 and sys.argv[1] == "local":
+    is_local = True
+
+# Import 
+def get_environment():
+    if is_local:
+        from retro_contest.local import make
+        env = make(game='SonicTheHedgehog-Genesis', state='LabyrinthZone.Act1')
+    else:
+        import gym_remote.exceptions as gre
+        import gym_remote.client as grc
+        env = grc.RemoteEnv('tmp/sock')
+    return env
 
 
 def main():
-    env = make(game='SonicTheHedgehog-Genesis', state='LabyrinthZone.Act1')
+    env = get_environment()
     obs = env.reset()
     while True:
-        obs, rew, done, info = env.step(env.action_space.sample())
-        env.render()
+        action = env.action_space.sample()
+        action[7] = 1 # Always run right
+        obs, rew, done, info = env.step(action)
+        if is_local:
+            env.render()
         if done:
             obs = env.reset()
 
