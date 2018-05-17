45dfbe4242cb0efac6cbd2961aff5b576c4d91b5
Patrick Manion <patrickjmanion@gmail.com>
2018-05-16 22:37:48 -0500

    Add model/config save/load functions to retro_s3

--- cnn-agent/retro_s3.py
+++ cnn-agent/retro_s3.py
@@ -1,5 +1,6 @@
 import boto3
 import io
+import torch
 
 class RetroS3Client:
 
@@ -9,11 +10,12 @@ class RetroS3Client:
         self.root_dir = root_dir
         self.s3_client = boto3.client('s3')
 
-    def save_from_buffer(self, buffer, name):
+    def save_from_buffer(self, buffer, file_name):
+        ''' Save a buffer onto S3 with the given file name '''
         self.s3_client.put_object(
             Body = buffer,
             Bucket = self.bucket,
-            Key = self.root_dir + model_name)
+            Key = self.root_dir + file_name)
 
     def load_to_buffer(self, model_name):
         ''' Load model off S3 into a buffer '''
@@ -22,4 +24,25 @@ class RetroS3Client:
             Key = self.root_dir + model_name)
 
         buffer = io.BytesIO(initial_bytes = response['Body'].read())
+        return buffer
+
+    def load_model_config_buffer(self, model_name):
+        ''' Load a (model, config) pair off S3 '''
+        buffer = self.load_to_buffer(model_name)
+        loaded_dict = torch.load(buffer)
+        return loaded_dict['model'], loaded_dict['config']
+
+    def save_model(self, model, config, model_name):
+        ''' Store a (model, config) pair directly onto S3 '''
+        buffer = io.BytesIO()
+        torch.save({
+            'model': self.input_to_buffer(model),
+            'config': self.input_to_buffer(config)
+        }, buffer)
+        self.save_from_buffer(buffer, model_name)
+
+    def input_to_buffer(self, input):
+        ''' Save the input into a buffer using its .save() method '''
+        buffer = io.BytesIO()
+        input.save(buffer)
         return buffer
