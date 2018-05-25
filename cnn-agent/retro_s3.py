ac1e15c582e3cfc6a9106667e9aa83434cfcd075
Patrick Manion <patrickjmanion@gmail.com>
2018-05-24 22:13:15 -0500

    Clean up retro_s3 design

--- cnn-agent/retro_s3.py
+++ cnn-agent/retro_s3.py
@@ -1,14 +1,17 @@
 import boto3
 import io
 import torch
+import csv
 
 class RetroS3Client:
 
     def __init__(self, bucket = 'retro-competition-8bitbandit',
                         root_dir = 'model_outputs/'):
+        self.s3_client = boto3.client('s3')
         self.bucket = bucket
         self.root_dir = root_dir
-        self.s3_client = boto3.client('s3')
+        if self.root_dir[-1] != '/': # Add trailling / if not present
+            self.root_dir += '/'
 
     def save_from_buffer(self, buffer, file_name):
         ''' Save a buffer onto S3 with the given file name '''
@@ -17,6 +20,26 @@ class RetroS3Client:
             Body = buffer,
             Bucket = self.bucket,
             Key = self.root_dir + file_name)
+
+    def save_memory(self, list_of_dicts, file_name):
+        ''' Store a memory (list of dictionaries) directly onto S3 '''
+        with io.StringIO() as str_buffer:
+            dw = csv.DictWriter(str_buffer, list_of_dicts[0].keys())
+            dw.writerows(list_of_dicts)
+            self.s3_client.put_object(
+                Body = str_buffer.getvalue().encode('utf-8'),
+                Bucket = self.bucket,
+                Key = self.root_dir + file_name)
+
+    def save_model(self, model, config, file_name):
+        ''' Store a (model, config) pair directly onto S3 '''
+        with io.BytesIO() as buffer:
+            torch.save({
+                'model': self.input_to_buffer(model),
+                'config': self.input_to_buffer(config)
+            }, buffer)
+            self.save_buffer(buffer, file_name)
+
 
     def load_to_buffer(self, model_name):
         ''' Load model off S3 into a buffer '''
@@ -46,12 +69,3 @@ class RetroS3Client:
         config_buffer.seek(0)
 
         return model_buffer, config_buffer
-
-    def save_model(self, model, config, model_name):
-        ''' Store a (model, config) pair directly onto S3 '''
-        buffer = io.BytesIO()
-        torch.save({
-            'model': self.input_to_buffer(model),
-            'config': self.input_to_buffer(config)
-        }, buffer)
-        self.save_from_buffer(buffer, model_name)
